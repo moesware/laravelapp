16c38070bd16117488b59d0a3d9f2fca9175c310
moesware <moesware@hotmail.com>
2018-11-30 21:43:08 +0100

    title2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    999games
+                    laravel
                 </div>
 
                 <div class="links">
